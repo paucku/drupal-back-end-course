9a50b8017a2097a3913340475b5cf580ffc894e8
Raicho Nikolov <raicho.nikolov@gmail.com>
2016-03-21 11:36:32 +0200

    Added files via upload
    Added example and experimental modules

---
+++ hooks_homework/modulea/modulea.api.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * @file
+ * Hooks provided by the Modulea module.
+ */
+
+/**
+ * @defgroup mobulea_api_hooks Moduleb API Hooks
+ * @{
+ * Functions to define and modify something.
+ *
+ * Functions to allow doing something.
+ *
+ * Here is a list of the hooks that are invoked during some operations:
+ * - Saying hello:
+ *   - hook_helloworld()
+ * @}
+ */
+
+/**
+ * @addtogroup hooks
+ * @{
+ */
+
+/**
+ * Does something.
+ *
+ * This hook enables the functionality to do something.
+ *
+ * @param $param
+ *   The first param.
+ * @param $another_param
+ *   The second param.
+ * @param $third
+ *   The third param.
+ *
+ * @return
+ *   Return the param that matches the condition.
+ */
+function hook_config($param, $another_param, $third) {
+
+}
+
+/**
+ * @} End of "addtogroup hooks".
+ */
